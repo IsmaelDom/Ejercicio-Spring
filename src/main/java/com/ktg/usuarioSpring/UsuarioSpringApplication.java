9492894f965fffdc9d2e4c0bd8f9837b2460e674
Ismael <ismaeldom11@gmail.com>
2020-12-22 13:36:36 -0600

    Creacion del Proyecto

---
+++ src/main/java/com/ktg/usuarioSpring/UsuarioSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ktg.usuarioSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktg/usuarioSpring/UsuarioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktg.usuarioSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
