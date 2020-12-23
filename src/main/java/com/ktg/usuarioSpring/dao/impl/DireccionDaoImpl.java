6c36f8abce3d0c114782e4efed241fbdd28a89d4
Ismael <ismaeldom11@gmail.com>
2020-12-23 13:21:14 -0600

    Se crea la implementación de las interfaces en donde ya realiza el CRUD, tanto para el usuario como para la dirección.

---
+++ src/main/java/com/ktg/usuarioSpring/dao/impl/DireccionDaoImpl.java
@@ -0,0 +1,55 @@
+package com.ktg.usuarioSpring.dao.impl;
+
+import com.ktg.usuarioSpring.dao.IDireccionDao;
+import com.ktg.usuarioSpring.model.entity.Direccion;
+import com.ktg.usuarioSpring.model.entity.Usuario;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Transactional
+@Repository
+public class DireccionDaoImpl implements IDireccionDao {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Transactional
+    @Override
+    public List<Direccion> getDireccion() {
+        String hql = "FROM Direccion as d";
+        return (List<Direccion>) entityManager.createQuery(hql).getResultList();
+    }
+
+    @Transactional
+    @Override
+    public Direccion getDireccionById(long id) {
+        return entityManager.find(Direccion.class, id);
+    }
+
+    @Transactional
+    @Override
+    public Direccion registrar(Direccion direccion) {
+        entityManager.merge(direccion);
+        return direccion;
+    }
+
+    @Transactional
+    @Override
+    public Direccion editar(Direccion direccion) {
+        entityManager.merge(direccion);
+        return direccion;
+    }
+
+    @Transactional
+    @Override
+    public String eliminar(long id) {
+        Direccion dir = getDireccionById(id);
+        entityManager.remove(dir);
+        String eliminado = "Dirección Eliminada";
+        return eliminado;
+    }
+}
---
+++ src/main/java/com/ktg/usuarioSpring/dao/impl/UsuarioDaoImpl.java
@@ -0,0 +1,54 @@
+package com.ktg.usuarioSpring.dao.impl;
+
+import com.ktg.usuarioSpring.dao.IUsuarioDao;
+import com.ktg.usuarioSpring.model.entity.Usuario;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Transactional
+@Repository
+public class UsuarioDaoImpl implements IUsuarioDao {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Transactional
+    @Override
+    public List<Usuario> getUsuarios(){
+        String hql = "FROM Usuario as u";
+        return (List<Usuario>) entityManager.createQuery(hql).getResultList();
+    }
+
+    @Transactional
+    @Override
+    public Usuario getUsuarioById(long id){
+        return entityManager.find(Usuario.class, id);
+    }
+
+    @Transactional
+    @Override
+    public Usuario registrar(Usuario user){
+        entityManager.merge(user);
+        return user;
+    }
+
+    @Transactional
+    @Override
+    public Usuario editar(Usuario user){
+        entityManager.merge(user);
+        return user;
+    }
+
+    @Transactional
+    @Override
+    public String eliminar(long id){
+        Usuario usuario = getUsuarioById(id);
+        entityManager.remove(usuario);
+        String eliminado = "Usuario Eliminado";
+        return eliminado;
+    }
+}
