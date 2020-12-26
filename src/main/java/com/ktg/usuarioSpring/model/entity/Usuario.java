8c7da9b80b1faa814171aef90410680506d7b244
Ismael <ismaeldom11@gmail.com>
2020-12-26 16:13:59 -0600

    Se agrega la anotación de solo escritura para la contraseña en la entidad usuario.

--- src/main/java/com/ktg/usuarioSpring/model/entity/Usuario.java
+++ src/main/java/com/ktg/usuarioSpring/model/entity/Usuario.java
@@ -1,6 +1,7 @@
 package com.ktg.usuarioSpring.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
 
@@ -23,6 +24,7 @@ public class Usuario {
     private String apellido;
 
     //@JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(nullable = false)
     private String password;
 
