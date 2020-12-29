6efc6c6abe9eaba07c2a7a54710895cf8628a859
Ismael <ismaeldom11@gmail.com>
2020-12-29 15:22:02 -0600

    Se agregan validaciones para buscar un usario por id y para eliminar.

--- src/main/java/com/ktg/usuarioSpring/controllers/DireccionController.java
+++ src/main/java/com/ktg/usuarioSpring/controllers/DireccionController.java
@@ -2,19 +2,24 @@ package com.ktg.usuarioSpring.controllers;
 
 import com.ktg.usuarioSpring.model.entity.Direccion;
 import com.ktg.usuarioSpring.services.DireccionService;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 //Anotación que indica que es un servicio REST
 @RestController
 //URL
 @RequestMapping("direccion")
-
+@Log
 public class DireccionController {
 
     @Autowired
@@ -41,8 +46,16 @@ public class DireccionController {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    DireccionUserDTO getFullDireccionById(@PathVariable long id){
-        return direccionService.getFullDireccion(id);
+    ResponseEntity<?> getFullDireccionById(@PathVariable long id){
+        DireccionUserDTO dir = direccionService.getFullDireccion(id);
+        Map<String, Object> resp = new HashMap<>();
+
+        if(dir == null){
+            resp.put("mensaje", "El usuario con id: " + id +" no existe en la base de datos.");
+            return new ResponseEntity<Map<String, Object>>(resp, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<DireccionUserDTO>(dir, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
@@ -50,7 +63,7 @@ public class DireccionController {
         return direccionService.registrar(direccion, resValida);
     }
 
-    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     Direccion editar(@Valid @RequestBody Direccion direccion, BindingResult resValida){
         return direccionService.editar(direccion, resValida);
@@ -58,7 +71,15 @@ public class DireccionController {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.OK)
-    void eliminar(@PathVariable long id){
+    ResponseEntity<?> eliminar(@PathVariable long id){
+        DireccionUserDTO dir = direccionService.getFullDireccion(id);
+        Map<String, Object> resp = new HashMap<>();
+        if(dir == null){
+            resp.put("mensaje", "El usuario con id: " + id +" no existe en la base de datos.");
+            return new ResponseEntity<Map<String, Object>>(resp, HttpStatus.NOT_FOUND);
+        }
         direccionService.eliminar(id);
+        resp.put("mensaje", "Usuario con id: " + id + " eliminado.");
+        return new ResponseEntity<Map<String, Object>>(resp, HttpStatus.OK);
     }
 }
