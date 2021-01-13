f6ad0d3f81ba3586b6a230d916700bffe0e26dd3
Ismael <ismaeldom11@gmail.com>
2021-01-13 13:26:40 -0600

    Se agrega url direcciones.

--- src/main/java/com/ktg/usuarioSpring/security/SecurityConfig.java
+++ src/main/java/com/ktg/usuarioSpring/security/SecurityConfig.java
@@ -60,9 +60,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                  .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                  .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                 .authorizeRequests().antMatchers("/auth/login").permitAll()
-                 //.antMatchers("/direcciones").permitAll()
-                 .anyRequest().authenticated();
+                 .authorizeRequests().antMatchers("/auth/login","/auth/signup").permitAll()
+                 .antMatchers("/direcciones").permitAll().anyRequest().authenticated();
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
