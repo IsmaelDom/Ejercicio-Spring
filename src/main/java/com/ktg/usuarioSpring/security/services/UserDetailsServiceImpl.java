d73d05efbdd8ab59b9d51b028f2c9fd600ffc7a2
Ismael <ismaeldom11@gmail.com>
2021-01-13 10:20:55 -0600

    Se mueve la clase UserDetailsServiceImpl a la carpeta de services.

--- src/main/java/com/ktg/usuarioSpring/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/ktg/usuarioSpring/security/services/UserDetailsServiceImpl.java
@@ -1,7 +1,8 @@
-package com.ktg.usuarioSpring.security;
+package com.ktg.usuarioSpring.security.services;
 
 import com.ktg.usuarioSpring.dao.IUsuarioDao;
 import com.ktg.usuarioSpring.model.entity.Usuario;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -10,6 +11,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Level;
+
+@Log
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
@@ -18,16 +22,16 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         Usuario usuario = usuarioDao.getUsuarioLogin(s);
+        log.log(Level.INFO,"UserDetailsServiceImp " + usuario);
         User.UserBuilder builder = null;
 
         if (usuario != null){
             builder = User.withUsername(s);
             builder.disabled(false);
             builder.password(usuario.getPassword());
-            builder.authorities(new SimpleGrantedAuthority("user"));
         }else{
             return null;
         }
-        return builder.build();
+        return UserDetailsImpl.build(usuario);
     }
 }
