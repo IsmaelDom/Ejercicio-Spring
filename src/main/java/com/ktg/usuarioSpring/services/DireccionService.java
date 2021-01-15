078c93dd3ff20e0a0789c0bc32a583ef96ef455d
Ismael <ismaeldom11@gmail.com>
2021-01-15 12:27:14 -0600

    Se agregan métodos con validaciones en el service.

--- src/main/java/com/ktg/usuarioSpring/services/DireccionService.java
+++ src/main/java/com/ktg/usuarioSpring/services/DireccionService.java
@@ -1,5 +1,6 @@
 package com.ktg.usuarioSpring.services;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.ktg.usuarioSpring.controllers.UsuariosDTO;
 import com.ktg.usuarioSpring.dao.IDireccionDao;
 import com.ktg.usuarioSpring.controllers.DireccionUserDTO;
@@ -7,7 +8,6 @@ import com.ktg.usuarioSpring.model.entity.Direccion;
 import com.ktg.usuarioSpring.model.entity.Usuario;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -82,45 +82,10 @@ public class DireccionService {
                 || direccion.getMunicipio().isEmpty() || direccion.getUsuario().getCorreo().isEmpty()
                 || direccion.getUsuario().getEdad() <= 0 || direccion.getUsuario().getApellido().isEmpty()
                 || direccion.getUsuario().getNombre().isEmpty() || direccion.getUsuario().getPassword().isEmpty()) {
-            if (direccion.getEstado().isEmpty() || direccion.getEstado().trim().isEmpty()) {
-                result.put("estado", "Ingrese un estado.");
-            }
-            if (direccion.getReferencia().isEmpty() || direccion.getReferencia().trim().isEmpty()) {
-                result.put("referencia", "Ingrese una referencia.");
-            }
-            if (direccion.getCp().isEmpty() || direccion.getCp().trim().isEmpty()) {
-                result.put("cp", "Ingrese un código postal.");
-            }
-            if (direccion.getNo_exterior().isEmpty()) {
-                result.put("no_exterior", "Ingrese un No. Exterior.");
-            }
-            if (direccion.getCalle().isEmpty()) {
-                result.put("calle", "Ingrese una calle.");
-            }
-            if (direccion.getMunicipio().isEmpty()) {
-                result.put("municipio", "Ingrese un municipio.");
-            }
-            if (direccion.getUsuario().getCorreo().isEmpty()) {
-                result.put("correo", "Ingrese un correo.");
-            } else if (!validaCorreo(direccion.getUsuario().getCorreo())) {
-                result.put("correo", "Ingrese un correo valido.");
-            }
-            if (direccion.getUsuario().getEdad() <= 0) {
-                result.put("edad", "Ingrese una edad.");
-            }
-            if (direccion.getUsuario().getApellido().isEmpty()) {
-                result.put("apellido", "Ingrese un apellido.");
-            }
-            if (direccion.getUsuario().getNombre().isEmpty()) {
-                result.put("nombre", "Ingrese un nombre.");
-            }
-            if (direccion.getUsuario().getPassword().isEmpty()) {
-                result.put("password", "Ingrese una contraseña.");
-            }
+            result = validaDatos(direccion);
             log.log(Level.SEVERE, "####### Error al Insertar Usuario con Dirección #####");
             log.log(Level.SEVERE, "Hay valores nulos, errores: ");
             log.log(Level.SEVERE, result.toString());
-            return result;
         }else{
             //Se encripta contraseña
             String pass = encoder.encode(direccion.getUsuario().getPassword());
@@ -132,14 +97,74 @@ public class DireccionService {
 
             log.log(Level.INFO, "####### Usuario con Dirección Insertado Correctamente");
             log.log(Level.INFO, dir.toString());
-            return result;
         }
+        return result;
     }
 
     private boolean validaCorreo(String email){
         String regexp = "^[a-zA-Z0-9]+[a-zA-Z0-9_.+-]+?@[a-zA-Z0-9-]+\\.[a-zA-Z0-9]+[.a-zA-Z]{0,3}$";
-        log.log(Level.INFO,"######Método validaCorreo devuelve: " + Pattern.matches(regexp, email) + "######");
+        log.log(Level.INFO,"###### Método validaCorreo devuelve: " + Pattern.matches(regexp, email) + " ######");
         return Pattern.matches(regexp, email);
+    }
+
+    private boolean validaEnteros(int numero){
+        String regexp = "^[1-9][0-9]?[0-9]?$";
+        String convertirInt = String.valueOf(numero);
+        log.log(Level.INFO,"###### Método validaEnteros devuelve: " + Pattern.matches(regexp, convertirInt) + " ######");
+        return Pattern.matches(regexp, convertirInt);
+    }
+
+    private boolean validaEnterosConString(String cadena){
+        String regexp = "^\\d+$";
+        log.log(Level.INFO,"###### Método validaEnterosConString devuelve: " + Pattern.matches(regexp, cadena) + " ######");
+        return Pattern.matches(regexp, cadena);
+    }
+
+    private Map<String, Object> validaDatos(Direccion direccion){
+        Map<String, Object> result = new HashMap<>();
+        if (direccion.getEstado().isEmpty()) {
+            result.put("estado", "Ingrese un estado.");
+        }
+        if (direccion.getReferencia().isEmpty() || direccion.getReferencia().trim().isEmpty()) {
+            result.put("referencia", "Ingrese una referencia.");
+        }
+        if (direccion.getCp().isEmpty()) {
+            result.put("cp", "Ingrese un código postal.");
+        }
+        if (direccion.getNo_exterior().isEmpty()) {
+            result.put("no_exterior", "Ingrese un No. Exterior.");
+        }else if(!validaEnterosConString(direccion.getNo_exterior())){
+            result.put("no_exterior", "Ingrese solo números para el no. exterior.");
+        }
+        if (direccion.getCalle().isEmpty()) {
+            result.put("calle", "Ingrese una calle.");
+        }
+        if (direccion.getMunicipio().isEmpty()) {
+            result.put("municipio", "Ingrese un municipio.");
+        }
+        if (direccion.getUsuario().getCorreo().isEmpty()) {
+            result.put("correo", "Ingrese un correo.");
+        } else if (!validaCorreo(direccion.getUsuario().getCorreo())) {
+            result.put("correo", "Ingrese un correo valido.");
+        }
+        if (direccion.getUsuario().getEdad() <= 0) {
+            result.put("edad", "Ingrese una edad.");
+        } else if(!validaEnteros(direccion.getUsuario().getEdad())){
+                result.put("edad", "Ingrese solo números para la edad.");
+        }
+        if (direccion.getUsuario().getApellido().isEmpty()) {
+            result.put("apellido", "Ingrese un apellido.");
+        }
+        if (direccion.getUsuario().getNombre().isEmpty()) {
+            result.put("nombre", "Ingrese un nombre.");
+        }
+        if (direccion.getUsuario().getPassword().isEmpty()) {
+            result.put("password", "Ingrese una contraseña.");
+        }else if(direccion.getUsuario().getPassword().length() < 5){
+            result.put("password", "La contraseña debe tener más de 5 caracteres.");
+        }
+
+        return result;
     }
 
     public Direccion editar(Direccion direccion){
