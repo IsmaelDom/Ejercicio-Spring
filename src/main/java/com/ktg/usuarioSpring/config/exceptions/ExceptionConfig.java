52cf259ca7cdaef488ddc489a4a2ebe5e65219fc
Ismael <ismaeldom11@gmail.com>
2021-01-17 17:51:52 -0600

    Se agrega una excepción por si el usuario cambia el id por una letra.

--- src/main/java/com/ktg/usuarioSpring/config/exceptions/ExceptionConfig.java
+++ src/main/java/com/ktg/usuarioSpring/config/exceptions/ExceptionConfig.java
@@ -34,27 +34,44 @@ public class ExceptionConfig {
         // convert errors to standard string
         StringBuilder errorMessage = new StringBuilder();
         fieldErrors.forEach(f -> errorMessage.append(f.getDefaultMessage() +  " "));
-        log.log(Level.SEVERE, "#### Errores-Clase Exception: " + fieldErrors.iterator().next().getDefaultMessage());
+        log.log(Level.SEVERE, "#### Errores-Metodo MethodArgumentNotValidException: " + fieldErrors.iterator().next().getDefaultMessage());
 
         // return error info object with standard json
         ErrorInfo errorInfo = new ErrorInfo(errorMessage.toString(), HttpStatus.BAD_REQUEST.value(), request.getRequestURI());
         return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler({MethodArgumentTypeMismatchException.class,
-                        DuplicateKeyException.class,
-                        HttpRequestMethodNotSupportedException.class,
-                        HttpMessageNotReadableException.class
-                        })
-    public ResponseEntity<ErrorInfo> MethodArgumentTypeMismatchException(HttpServletRequest request, Exception e) {
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorInfo> MethodArgumentTypeMismatchException(HttpServletRequest request, MethodArgumentTypeMismatchException e) {
 
         // get spring errors
         String result = e.getMessage();
 
         // convert errors to standard string
         StringBuilder errorMessage = new StringBuilder();
-        errorMessage.append("Error: Ha ocurrido un error por favor revise el log para más detalle.");
-        log.log(Level.SEVERE, "#### Error-Clase Exception: " + result);
+        errorMessage.append("El campo " + e.getName() + " no puede ser " + e.getValue());
+        log.log(Level.SEVERE, "#### Error-Metodo MethodArgumentTypeMismatchException: " + result);
+        log.log(Level.SEVERE, "#### Error-Causa más especifica: " + e.getCause());
+        log.log(Level.SEVERE, "#### Error el " + e.getName() + " debe ser de tipo: " + e.getRequiredType());
+
+        // return error info object with standard json
+        ErrorInfo errorInfo = new ErrorInfo(errorMessage.toString(), HttpStatus.BAD_REQUEST.value(), request.getRequestURI());
+        return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({DuplicateKeyException.class,
+                        HttpRequestMethodNotSupportedException.class,
+                        HttpMessageNotReadableException.class
+                        })
+    public ResponseEntity<ErrorInfo> DuplicateKeyException(HttpServletRequest request, Exception e) {
+
+        // get spring errors
+        String result = e.getMessage();
+
+        // convert errors to standard string
+        StringBuilder errorMessage = new StringBuilder();
+        errorMessage.append("Error: Ha ocurrido un error por favor revise el log para más detalle: " + result);
+        log.log(Level.SEVERE, "#### Error-Método DuplicateKeyException: " + result);
         log.log(Level.SEVERE, "#### Error-Causa: " + e.getCause());
 
         // return error info object with standard json
@@ -71,7 +88,7 @@ public class ExceptionConfig {
         // convert errors to standard string
         StringBuilder errorMessage = new StringBuilder();
         errorMessage.append("Error: " + e.getCause().getMessage() + ". Causa: " + e.getCause());
-        log.log(Level.SEVERE, "#### Error-Clase Exception: " + result);
+        log.log(Level.SEVERE, "#### Error-Método DataAccessException: " + result);
         log.log(Level.SEVERE, "#### Error-Causa más especifica: " + e.getLocalizedMessage());
 
         // return error info object with standard json
@@ -85,7 +102,7 @@ public class ExceptionConfig {
         // convert errors to standard string
         StringBuilder errorMessage = new StringBuilder();
         errorMessage.append("Error: No esta autorizado para esta acción");
-        log.log(Level.SEVERE, "#### Error-Clase Exception: De tipo Unauthorized o Acceso denegado");
+        log.log(Level.SEVERE, "#### Error-Método Unauthorized: De tipo Unauthorized o Acceso denegado");
         log.log(Level.SEVERE, "#### Error: No esta autorizado o no tiene privilegios para esta acción");
 
         // return error info object with standard json
@@ -102,9 +119,9 @@ public class ExceptionConfig {
         // convert errors to standard string
         StringBuilder errorMessage = new StringBuilder();
         errorMessage.append("Contraseña o correo incorrecto");
-        log.log(Level.SEVERE, "#### Error-Clase AuthenticationException: " + result);
+        log.log(Level.SEVERE, "#### Error-Método AuthenticationException: " + result);
         log.log(Level.SEVERE, "#### Error-Causa más especifica: " + e.getCause());
-        log.log(Level.SEVERE, "#### Error Localización: " + e.getLocalizedMessage());
+        log.log(Level.SEVERE, "#### Error de credenciales: Contraseña o correo incorrecto");
 
         // return error info object with standard json
         ErrorInfo errorInfo = new ErrorInfo(errorMessage.toString(), HttpStatus.UNAUTHORIZED.value(), request.getRequestURI());
@@ -120,7 +137,7 @@ public class ExceptionConfig {
         // convert errors to standard string
         StringBuilder errorMessage = new StringBuilder();
         errorMessage.append("Error: " + e.getCause());
-        log.log(Level.SEVERE, "#### Error-Clase Exception excepción de tipo Exception: " + result);
+        log.log(Level.SEVERE, "#### Error-Método Exception excepción de tipo Exception: " + result);
         log.log(Level.SEVERE, "#### Error-Causa más especifica: " + e.getCause());
         log.log(Level.SEVERE, "#### Error Localización: " + e.getLocalizedMessage());
 
